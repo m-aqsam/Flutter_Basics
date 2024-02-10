4d2fa7f10493e2c6f28610e81394ad40458fb680
m_aqsam <maqsam1155@gmail.com>
2024-02-10 23:59:42 +0500

    update

--- flutter_widgets/lib/01_Container_Widget.dart
+++ flutter_widgets/lib/01_Container_Widget.dart
@@ -8,41 +8,36 @@ class Container_Widget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
           //backgroundColor: Colors.deepPurple[200],
           appBar: AppBar(
             backgroundColor: Colors.deepPurple,
             title: Text("Dashboard"),
           ),
           body: Center(
-            child: Container(
-              height: 300,
-              width: 300,
-              color: Colors.deepPurple,
-              decoration:
-                  BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.shade500,
-                      offset: Offset(4.0, 4.0),
-                      blurRadius: 15.0,
-                      spreadRadius: 1.0, 
-                    ),
-                    BoxShadow(
-                      color: Colors.white,
-                      offset: Offset(-4.0, -4.0),
-                      blurRadius: 15.0,
-                      spreadRadius: 1.0,  
-                    ),
-
-                    
-                  ]
-                  ),
-                  
-                  
+              child: Container(
+            height: 300,
+            width: 300,
+            color: Colors.deepPurple,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade500,
+                  offset: Offset(4.0, 4.0),
+                  blurRadius: 15.0,
+                  spreadRadius: 1.0,
+                ),
+                BoxShadow(
+                  color: Colors.white,
+                  offset: Offset(-4.0, -4.0),
+                  blurRadius: 15.0,
+                  spreadRadius: 1.0,
+                ),
+              ],
+            ),
           )),
-    );
+        ));
   }
 }
