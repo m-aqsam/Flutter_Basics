5a53541046fbe5801883ed255f587d00820fc256
m_aqsam <maqsam1155@gmail.com>
2024-02-12 23:46:04 +0500

    update 1

---
+++ flutter_widgets/lib/02_Expanded_Widget.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: camel_case_types, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Expanded_Widget extends StatelessWidget {
+  const Expanded_Widget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
