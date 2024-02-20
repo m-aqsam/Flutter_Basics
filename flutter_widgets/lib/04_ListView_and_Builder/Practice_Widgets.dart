b369373e1dded214e1a363a75809ae7cd8671a6f
m_aqsam <maqsam1155@gmail.com>
2024-02-20 23:52:41 +0500

    update

---
+++ flutter_widgets/lib/04_ListView_and_Builder/Practice_Widgets.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_constructors, camel_case_types
+
+import 'package:flutter/material.dart';
+
+class Practice_Widgets extends StatelessWidget {
+  const Practice_Widgets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Practice"),
+        ),
+      ),
+    );
+  }
+}
