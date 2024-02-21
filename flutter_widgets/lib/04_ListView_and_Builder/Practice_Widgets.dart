99bdf308b8246569eca0a5c85df2ac63fa3ddf91
m_aqsam <maqsam1155@gmail.com>
2024-02-21 21:37:36 +0500

    Update Practice Widget

--- flutter_widgets/lib/04_ListView_and_Builder/Practice_Widgets.dart
+++ flutter_widgets/lib/04_ListView_and_Builder/Practice_Widgets.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, camel_case_types
+// ignore_for_file: prefer_const_constructors, camel_case_types, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -8,10 +8,22 @@ class Practice_Widgets extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.deepPurple[300],
           title: Text("Practice"),
         ),
+        body: Center(
+            child: CircleAvatar(
+          backgroundColor: Colors.red,
+          foregroundColor: Colors.white,
+          radius: 100,
+          child: Text(
+            'A',
+            style: TextStyle(fontSize: 100),
+          ),
+        )),
       ),
     );
   }
