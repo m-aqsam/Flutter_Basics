038943ea192cc9cc431934c1fda9ab2a3037b357
m_aqsam <maqsam1155@gmail.com>
2024-02-29 15:42:39 +0500

    Home Final Update

--- flutter_widgets/lib/09_Bottom_Nav_Bar/Home.dart
+++ flutter_widgets/lib/09_Bottom_Nav_Bar/Home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class MyHome extends StatelessWidget {
@@ -5,6 +7,14 @@ class MyHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Text(
+        'Home',
+        style: TextStyle(
+          fontSize: 50,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
   }
 }
