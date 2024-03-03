7b19797de0ff203d359caa4067a48256d94fb0a9
m_aqsam <maqsam1155@gmail.com>
2024-03-03 23:53:56 +0500

    Update

--- flutter_widgets/lib/10_AppBar.dart
+++ flutter_widgets/lib/10_AppBar.dart
@@ -1,9 +1,9 @@
-// ignore_for_file: unused_element, prefer_const_constructors
+// ignore_for_file: unused_element, prefer_const_constructors, camel_case_types
 
 import 'package:flutter/material.dart';
 
-class _AppBar extends StatelessWidget {
-  const _AppBar({super.key});
+class My_AppBar extends StatelessWidget {
+  const My_AppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +12,7 @@ class _AppBar extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text('App Bar'),
-          backgroundColor: Colors.deepPurple[500],
+          backgroundColor: Colors.deepPurple[500], 
         ),
       ),
     );
