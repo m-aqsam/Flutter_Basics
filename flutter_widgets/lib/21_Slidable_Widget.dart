898b69248a2ae1154ff9987cb6d01fc99f519e12
m_aqsam <maqsam1155@gmail.com>
2024-03-09 23:50:28 +0500

    Update

---
+++ flutter_widgets/lib/21_Slidable_Widget.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class Slideable_Widget extends StatefulWidget {
+  const Slideable_Widget({super.key});
+
+  @override
+  State<Slideable_Widget> createState() => _Slideable_WidgetState();
+}
+
+class _Slideable_WidgetState extends State<Slideable_Widget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+    );
+  }
+}
