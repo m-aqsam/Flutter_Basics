7ae867d4f214885d29e930e420d63db5ec91ed4c
m_aqsam <maqsam1155@gmail.com>
2024-03-12 23:46:04 +0500

    Add Splash Screen

---
+++ flutter_widgets/lib/Flutter Application Ui/Splash_Screen_Ui.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class Splash_Screen_Ui extends StatelessWidget {
+  const Splash_Screen_Ui({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[400],
+    );
+  }
+}
