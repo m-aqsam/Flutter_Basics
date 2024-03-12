7d7e6e6dc6675874ec0ddd25ac88f344ff3462ef
m_aqsam <maqsam1155@gmail.com>
2024-03-12 23:52:19 +0500

    update

--- flutter_widgets/lib/Flutter Application Ui/Splash_Screen_Ui.dart
+++ flutter_widgets/lib/Flutter Application Ui/Splash_Screen_Ui.dart
@@ -9,6 +9,13 @@ class Splash_Screen_Ui extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow[400],
+      body: Center(
+        child: Container(
+          height: 450,
+          width: 350,
+          color: Colors.black,
+        ),
+      ),
     );
   }
 }
