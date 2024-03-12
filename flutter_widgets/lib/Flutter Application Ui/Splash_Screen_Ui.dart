fb08efd3b86d7196c45fa223702e4e66506bec9e
m_aqsam <maqsam1155@gmail.com>
2024-03-13 01:37:53 +0500

    Add Splash Screen Ui

--- flutter_widgets/lib/Flutter Application Ui/Splash_Screen_Ui.dart
+++ flutter_widgets/lib/Flutter Application Ui/Splash_Screen_Ui.dart
@@ -1,20 +1,50 @@
-// ignore_for_file: camel_case_types
+// ignore_for_file: camel_case_types, prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 
-class Splash_Screen_Ui extends StatelessWidget {
+class Splash_Screen_Ui extends StatefulWidget {
   const Splash_Screen_Ui({super.key});
 
   @override
+  State<Splash_Screen_Ui> createState() => _Splash_Screen_UiState();
+}
+
+class _Splash_Screen_UiState extends State<Splash_Screen_Ui> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow[400],
-      body: Center(
-        child: Container(
-          height: 450,
-          width: 350,
-          color: Colors.black,
-        ),
+      backgroundColor: Colors.green,
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: Container(
+                  height: 80,
+                  width: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    image: DecorationImage(
+                      image: AssetImage("lib/images/avatar.png"),
+
+                      //fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.greenAccent[400],
+                  borderRadius: BorderRadius.circular(200),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
